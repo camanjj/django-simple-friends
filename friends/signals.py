3439860dcddf7fe3da1ee5a9d3fe1e2110b9fcdb
Cameron Jackson <jackson.cameron82@gmail.com>
2013-02-18 20:00:54 -0500

    more fixes

--- friends/signals.py
+++ friends/signals.py
@@ -32,7 +32,6 @@ def create_friendship_instance_post_syncdb(sender,
     from friends.models import Friendship
     created = 0
     print "Creating friendships"
-    #if settings.AUTH_USER_MODEL in created_models:
 	for user in User.objects.filter(friendship__isnull=True):
 		Friendship.objects.create(user=user)
 		created += 1
@@ -51,7 +50,6 @@ def create_userblock_instance_post_syncdb(sender,
     from friends.models import UserBlocks
     created = 0
     print "Creating user blocks"
-    #if settings.AUTH_USER_MODEL in created_models:
 	for user in User.objects.filter(user_blocks__isnull=True):
 		UserBlocks.objects.create(user=user)
 		created += 1
