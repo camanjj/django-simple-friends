e8286a2203212dadb45409e7e981185de5e68f3c
Cameron Jackson <jackson.cameron82@gmail.com>
2013-02-18 19:36:20 -0500

    changed signals for initial syncdb

--- friends/signals.py
+++ friends/signals.py
@@ -31,12 +31,12 @@ def create_friendship_instance_post_syncdb(sender,
     from friends.models import Friendship
     created = 0
     print "Creating friendships"
-    if settings.AUTH_USER_MODEL in created_models:
-        for user in settings.AUTH_USER_MODEL.objects.filter(friendship__isnull=True):
-            Friendship.objects.create(user=user)
-            created += 1
-            if verbosity >= 2:
-                print "Friendship created for %s" % user
+    #if settings.AUTH_USER_MODEL in created_models:
+	for user in settings.AUTH_USER_MODEL.objects.filter(friendship__isnull=True):
+		Friendship.objects.create(user=user)
+		created += 1
+		if verbosity >= 2:
+			print "Friendship created for %s" % user
     if verbosity >= 1:
         print "%d friendships created" % created
 
@@ -50,11 +50,11 @@ def create_userblock_instance_post_syncdb(sender,
     from friends.models import UserBlocks
     created = 0
     print "Creating user blocks"
-    if settings.AUTH_USER_MODEL in created_models:
-        for user in settings.AUTH_USER_MODEL.objects.filter(user_blocks__isnull=True):
-            UserBlocks.objects.create(user=user)
-            created += 1
-            if verbosity >= 2:
-                print "User block created for %s" % user
+    #if settings.AUTH_USER_MODEL in created_models:
+	for user in settings.AUTH_USER_MODEL.objects.filter(user_blocks__isnull=True):
+		UserBlocks.objects.create(user=user)
+		created += 1
+		if verbosity >= 2:
+			print "User block created for %s" % user
     if verbosity >= 1:
         print "%d user blocks created" % created
